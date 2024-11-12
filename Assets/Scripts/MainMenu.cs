2ac432f8ad9ac7902ee21058169a3a3ed6ca1735
Ninadacobra <ninadacobra@gmail.com>
2024-11-12 15:53:14 -0600

    Main menu is back

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Kyle Scene");
+        SceneManager.LoadScene("BasementLevel");
     }
 
     
