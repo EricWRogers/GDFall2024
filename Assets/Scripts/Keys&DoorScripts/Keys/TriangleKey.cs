a6c4aee4fce759e530718c0514805a04c30e1c25
Eli <therealanimegunman@gmail.com>
2024-11-20 20:19:21 -0600

    Created Key/Door Scripts for each Key/Door

---
+++ Assets/Scripts/Keys&DoorScripts/Keys/TriangleKey.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;  // Required for UI components
+
+public class TriangleKey : MonoBehaviour
+{
+    public bool hasTriangleKey = false;  // Tracks whether the player has picked up the Triangle Key
+    public TMP_Text pickupMessageText;  // Reference to the UI Text for feedback
+    public TMP_Text interactionText;    // Reference to the UI Text for interaction feedback
+    public float pickupRange = 3f;  // The distance at which the player can pick up the key
+
+    // New addition: Reference to the Image that will appear after the key is picked up
+    public Image triangleKeyImage;  
+
+    private GameObject player;  // Reference to the player GameObject
+
+    private void Start()
+    {
+        // Initially, the player does not have the key
+        if (pickupMessageText != null)
+        {
+            pickupMessageText.text = "";  // Hide any pickup message at the start
+        }
+
+        // Make sure the key image is hidden at the start
+        if (triangleKeyImage != null)
+        {
+            triangleKeyImage.enabled = false;  // Hide the triangle key image initially
+        }
+
+        // Find the player in the scene
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        // Check if player is close enough to the key and if the player has not already picked it up
+        if (player != null && !hasTriangleKey)
+        {
+            float distanceToKey = Vector3.Distance(player.transform.position, transform.position);
+
+            // Check if the player is within range to pick up the key
+            if (distanceToKey <= pickupRange)
+            {
+                // Show a message to prompt the player to press 'E' to pick up the key
+                if (pickupMessageText != null)
+                {
+                    pickupMessageText.text = "Press E to pick up";
+                }
+
+                // Wait for the player to press 'E'
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    PickUpTriangleKey();
+                }
+            }
+            else
+            {
+                // Hide the message if the player is out of range
+                if (pickupMessageText != null)
+                {
+                    pickupMessageText.text = "";
+                }
+            }
+        }
+    }
+
+    // Pick up the Triangle Key when the player presses 'E'
+    void PickUpTriangleKey()
+    {
+        // Set the hasTriangleKey flag to true
+        hasTriangleKey = true;
+
+        // Update the message to show key picked up
+        if (pickupMessageText != null)
+        {
+            pickupMessageText.text = "Triangle Key picked up!";
+        }
+
+        // Show the key image in the UI
+        if (triangleKeyImage != null)
+        {
+            triangleKeyImage.enabled = true;  // Make the triangle key image visible
+        }
+
+        // Hide the prompt after the key is picked up
+        if (pickupMessageText != null)
+        {
+            pickupMessageText.text = "";
+        }
+
+        // Deactivate the key object so it no longer interferes with the player
+        gameObject.SetActive(false);  // Deactivate the key object (but don't destroy it)
+
+        // Log for debugging purposes
+        Debug.Log("Triangle Key picked up!");
+    }
+}
